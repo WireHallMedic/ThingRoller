da1246ec5733151399ffc5e8f5dbd1d80f226753
WireHallMedic <michaelwidler@yahoo.com>
2022-08-05 12:05:05 -0700

    Move constants to own file

--- thing_roller.py
+++ thing_roller.py
@@ -7,20 +7,10 @@ import name_gen
 import deck
 import socket
 import subtable_main
+import constants
 
 # various regExes
-DICE_REG_EX = "\d+d\d+|advantage|disadvantage"
-INT_REG_EX = "\d+"
-OPERATOR_REG_EX = "[+\-/*x]"
-NON_OPERATOR_REG_EX = "[^+\-/\*x]"
-SHOULD_CALCULATE_REG_EX = "^[+\-\d]|^d\d|^advantage|^disadvantage|\d+df"
-SHOULD_INSERT_ONE_REG_EX = "^d\d"
-NUMBER_REG_EX = "\d+"
-FUDGE_DICE_REG_EX = "\d+df"
 
-NOT_YET_IMPLEMENTED_STR = ":warning: This feature is not yet implemented :warning:"
-MAX_DICE = 1000000
-ADMIN_NAME = "wire_hall_medic"
 
 # change cwd in case this is called from shell script
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
