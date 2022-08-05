8435ac16b4b9c68dbc21968031104c169d82a9de
WireHallMedic <michaelwidler@yahoo.com>
2022-08-05 10:21:54 -0700

    Add Fudge dice regex

--- thing_roller.py
+++ thing_roller.py
@@ -16,6 +16,7 @@ NON_OPERATOR_REG_EX = "[^+\-/\*x]"
 SHOULD_CALCULATE_REG_EX = "^[+\-\d]|^d\d|^advantage|^disadvantage"
 SHOULD_INSERT_ONE_REG_EX = "^d\d"
 NUMBER_REG_EX = "\d+"
+FUDGE_DICE_REG_EX = "\d+df"
 
 NOT_YET_IMPLEMENTED_STR = ":warning: This feature is not yet implemented :warning:"
 MAX_DICE = 1000000
