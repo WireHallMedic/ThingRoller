697890c55d7e8d4219c4042e4443395d7126f57b
WireHallMedic <michaelwidler@yahoo.com>
2022-10-12 13:21:09 -0700

    cast image to file

--- thing_roller.py
+++ thing_roller.py
@@ -142,7 +142,7 @@ async def on_message(message):
       cmd = "fate"
       roll_obj = fate_dice.roll()
       out_str = roll_obj[0]
-      out_file = File(roll_obj[1])
+      out_file = open(roll_obj[1])
    
    #roll some dice and/or calculate
    if re.search(SHOULD_CALCULATE_REG_EX, cmd) != None:
