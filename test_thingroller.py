6edb7207781475339fddc1fb1b177e3353f2fc05
Michael <MichaelWidler@yahoo.com>
2024-08-04 13:26:22 -0700

    Tweak tests.

--- test_thingroller.py
+++ test_thingroller.py
@@ -3,15 +3,15 @@
 import dice
 
 if __name__ == "__main__":
-   dice.resolve_dice_expression("3d6 + 5 - 1d4")
+   dice.resolve_dice_expression("3d6 + 5 - 1d4 / 5 * 4")
    dice.resolve_advantage()
    dice.resolve_disadvantage()
    dice.roll(6)
-   for i in range(100):
+   for i in range(1000):
       if dice.roll(6, True) > 6:
          break
       if i == 99:
-         raise Exception("No exploding 6 in a hundred tries") 
+         raise Exception("No exploding 6 in a thousand tries") 
    dice.roll_fudge_die()
    dice.roll_stat_block()
    
