d8aa5da8eff17360682019b69d560625e3e58b37
Michael <MichaelWidler@yahoo.com>
2024-08-09 09:57:28 -0700

    Correct tests.

--- test_thingroller.py
+++ test_thingroller.py
@@ -5,11 +5,11 @@ import random
 
 if __name__ == "__main__":
    random.seed(10000)
-   assert dice.resolve_dice_expression("3d6 + 5 - 1d4 / 5 * 4") is not None
+   assert dice.resolve_dice_expression("3d6 + 5 - 1d4 / 5 * 4") == "**Result: 10.4**\nRolled: (5, 3, 1) + 5 - (1) / 5 * 4"
    assert dice.resolve_dice_expression("3d6 + 5 j 1d4 / 5 * 4", suppress_exception_message = True) is None
-   assert dice.resolve_advantage() is not None
-   assert dice.resolve_disadvantage() is not None
-   assert dice.roll(6) is not None
+   assert dice.resolve_advantage() == [9, 'max(1, 9)']
+   assert dice.resolve_disadvantage() == [11, 'min(17, 11)']
+   assert dice.roll(6) == 2
    exploded_die = False
    for i in range(100):
       if dice.roll(2, True) > 2:
@@ -18,7 +18,3 @@ if __name__ == "__main__":
    assert exploded_die
    assert dice.roll_fudge_die() is not None
    assert dice.roll_stat_block() is not None
-   
-   print(dice.resolve_dice_expression("3d6 + 5 - 1d4 / 5 * 4"))
-   print(dice.resolve_dice_expression("10d4", exploding = True))
-   print(dice.resolve_dice_expression("d8"))
